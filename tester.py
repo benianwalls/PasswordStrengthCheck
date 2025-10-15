70e8acd584c0b832076af49e21c8e8356d569711
wallbe <wallbe@iu.edu>
2025-10-15 11:19:20 -0400

    length

--- tester.py
+++ tester.py
@@ -1,1 +1,16 @@
 import re
+# Strength rules
+'''
+Password length > 8
+Letter case - contains both uppercase and lowercase letters
+Numbers - contains atleast one digit
+Special characters - contains atleast one special character
+Common Passwrod check = not in common password list
+
+'''
+def check_length(password):
+    strength = 0
+    if len(password) >= 8:
+        strength += 1
+    else:
+        print ("password length should be greater than 8")
